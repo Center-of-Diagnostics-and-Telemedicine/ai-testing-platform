a4653d363a3305e49a6c79391362a2cceb3001d0
DmitriyValetov <dmitriy.valetov@phystech.edu>
2020-11-26 22:49:06 +0300

    added test for session token adding

--- db_base.py
+++ db_base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, Integer, ForeignKey, String, Float, Boolean, JSON, DateTime
+from sqlalchemy import Table, Column, Integer, ForeignKey, String, Float, Boolean, JSON, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -39,7 +39,7 @@ class dbManager:
         class Service(self.Base):
             __tablename__ = 'services'
             id = Column(Integer, primary_key=True, autoincrement=True)
-            name = Column(String)
+            name = Column(String, unique=True)
             token = Column(String)
 
             def __str__(self):
@@ -51,7 +51,7 @@ class dbManager:
         class Session_token(self.Base):
             __tablename__ = 'session_tokens'
             id = Column(Integer, primary_key=True, autoincrement=True)
-            service = Column(Integer)
+            service = Column(Integer, ForeignKey(Service.id))
             session_token = Column(String)
             issue_date = Column(DateTime)
             expiry_date = Column(DateTime)
@@ -66,9 +66,9 @@ class dbManager:
         class Test(self.Base):
             __tablename__ = 'testing'
             id = Column(Integer, primary_key=True, autoincrement=True)
-            session = Column(Integer)
-            dataset_title = Column(String)
-            dataset_file_id = Column(Integer)
+            session = Column(Integer, ForeignKey(Session_token.id))
+            dataset_title = Column(String, ForeignKey(Dataset.title))
+            dataset_file_id = Column(Integer, ForeignKey(Dataset.id))
             created = Column(DateTime, default=datetime.datetime.utcnow)
             retrieved = Column(DateTime)
             received = Column(DateTime)
@@ -120,7 +120,7 @@ class dbManager:
         return self.session
 
 
-    def add_service(self, name, token):
+    def insert_service(self, name, token):
         session = self.make_session()
         session.add(db.Service(
             name=name,
@@ -128,8 +128,25 @@ class dbManager:
         ))
         self.commit_and_close()
 
+    def insert_token(self, service_name, session_token):
+        # get service id
+        session = self.make_session()
+        found_service = session.query(db.Service).filter(db.Service.name == service_name).first()
+        if found_service is None:
+            raise ValueError(f"There is no service with name: {service_name}")
+        
+        # insert
+        session.add(self.Session_token(
+            service=found_service.id,
+            session_token=session_token,
+            issue_date=datetime.datetime.utcnow(),
+            expiry_date=datetime.datetime.utcnow()+datetime.timedelta(days=1),
+            active=True,
+        ))
+        self.commit_and_close()
 
-    def add_dataset_row(self, 
+
+    def insert_dataset_file(self, 
         title,
         filename,
         var1=None,
@@ -164,7 +181,7 @@ def test_service_insert(db):
     test_name = 'test_service'
     test_token = '123qwerty654'
 
-    db.add_service(test_name, test_token)
+    db.insert_service(test_name, test_token)
 
     session = db.make_session()
     services = session.query(db.Service).filter(db.Service.name == test_name).all()
@@ -176,22 +193,42 @@ def test_dataset_row_insert(db):
     test_title = 'test_title'
     test_filename = 'test_filename'
 
-    db.add_dataset_row(test_title, test_filename)
+    db.insert_dataset_file(test_title, test_filename)
 
     session = db.make_session()
     datatasets = session.query(db.Dataset).filter(db.Dataset.title == test_title).all()
     assert len(datatasets) == 1
     assert datatasets[0].filename == test_filename
 
-    
+
+def test_session_token_insert(db):
+    # insert 
+    service_name = 'test_service2'
+    service_token = 'service_token'
+    session_token = 'session_token'
+    db.insert_service(service_name, service_token)
+    db.insert_token(service_name, session_token)
+
+    # check
+    session = db.make_session()
+    service = session.query(db.Service).filter(db.Service.name == service_name).one()
+    the_token = session.query(db.Session_token).filter(db.Session_token.service == service.id).one()
+    assert the_token.session_token == session_token
+
 
 if __name__ == "__main__":
+    try:
+        os.remove(db_path)
+    except:
+        pass
+
     db_path = os.path.join(os.getcwd(), 'test_db.sqlite')
     db = dbManager(db_path)
     db.empty()
 
     test_service_insert(db)
     test_dataset_row_insert(db)
+    test_session_token_insert(db)
 
     os.remove(db_path)
     
